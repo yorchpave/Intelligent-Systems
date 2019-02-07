8085f937e19c16016ea973cc2a298ec085b7dd71
Jorge Adrián Padilla Velasco <jpadillavelasco95@gmail.com>
2019-02-06 22:43:35 -0600

    Create astar.py

---
+++ 8-puzzle-problem/astar.py
@@ -0,0 +1,230 @@
+import argparse
+import timeit
+import resource
+from state import State
+from heapq import heappush, heappop, heapify
+import itertools
+
+goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+goal_node = State
+initial_state = list()
+board_len = 0
+board_side = 0
+
+nodes_expanded = 0
+max_search_depth = 0
+max_frontier_size = 0
+
+moves = list()
+costs = set()
+
+def ast(start_state):
+
+    global max_frontier_size, goal_node, max_search_depth
+
+    explored, heap, heap_entry, counter = set(), list(), {}, itertools.count()
+
+    key = h(start_state)
+
+    root = State(start_state, None, None, 0, 0, key)
+
+    entry = (key, 0, root)
+
+    heappush(heap, entry)
+
+    heap_entry[root.map] = entry
+
+    while heap:
+
+        node = heappop(heap)
+
+        explored.add(node[2].map)
+
+        if node[2].state == goal_state:
+            goal_node = node[2]
+            return heap
+
+        neighbors = expand(node[2])
+
+        for neighbor in neighbors:
+
+            neighbor.key = neighbor.cost + h(neighbor.state)
+
+            entry = (neighbor.key, neighbor.move, neighbor)
+
+            if neighbor.map not in explored:
+
+                heappush(heap, entry)
+
+                explored.add(neighbor.map)
+
+                heap_entry[neighbor.map] = entry
+
+                if neighbor.depth > max_search_depth:
+                    max_search_depth += 1
+
+            elif neighbor.map in heap_entry and neighbor.key < heap_entry[neighbor.map][2].key:
+
+                hindex = heap.index((heap_entry[neighbor.map][2].key,
+                                     heap_entry[neighbor.map][2].move,
+                                     heap_entry[neighbor.map][2]))
+
+                heap[int(hindex)] = entry
+
+                heap_entry[neighbor.map] = entry
+
+                heapify(heap)
+
+        if len(heap) > max_frontier_size:
+            max_frontier_size = len(heap)
+
+def expand(node):
+
+    global nodes_expanded
+    nodes_expanded += 1
+
+    neighbors = list()
+
+    neighbors.append(State(move(node.state, 1), node, 1, node.depth + 1, node.cost + 1, 0))
+    neighbors.append(State(move(node.state, 2), node, 2, node.depth + 1, node.cost + 1, 0))
+    neighbors.append(State(move(node.state, 3), node, 3, node.depth + 1, node.cost + 1, 0))
+    neighbors.append(State(move(node.state, 4), node, 4, node.depth + 1, node.cost + 1, 0))
+
+    nodes = [neighbor for neighbor in neighbors if neighbor.state]
+
+    return nodes
+
+def move(state, position):
+
+    new_state = state[:]
+
+    index = new_state.index(0)
+
+    if position == 1:  # Up
+
+        if index not in range(0, board_side):
+
+            temp = new_state[index - board_side]
+            new_state[index - board_side] = new_state[index]
+            new_state[index] = temp
+
+            return new_state
+        else:
+            return None
+
+    if position == 2:  # Down
+
+        if index not in range(board_len - board_side, board_len):
+
+            temp = new_state[index + board_side]
+            new_state[index + board_side] = new_state[index]
+            new_state[index] = temp
+
+            return new_state
+        else:
+            return None
+
+    if position == 3:  # Left
+
+        if index not in range(0, board_len, board_side):
+
+            temp = new_state[index - 1]
+            new_state[index - 1] = new_state[index]
+            new_state[index] = temp
+
+            return new_state
+        else:
+            return None
+
+    if position == 4:  # Right
+
+        if index not in range(board_side - 1, board_len, board_side):
+
+            temp = new_state[index + 1]
+            new_state[index + 1] = new_state[index]
+            new_state[index] = temp
+
+            return new_state
+        else:
+            return None
+
+
+def h(state):
+
+    return sum(abs(b % board_side - g % board_side) + abs(b//board_side - g//board_side)
+               for b, g in ((state.index(i), goal_state.index(i)) for i in range(1, board_len)))
+
+def backtrace():
+
+    current_node = goal_node
+
+    while initial_state != current_node.state:
+
+        if current_node.move == 1:
+            movement = 'Up'
+        elif current_node.move == 2:
+            movement = 'Down'
+        elif current_node.move == 3:
+            movement = 'Left'
+        else:
+            movement = 'Right'
+
+        moves.insert(0, movement)
+        current_node = current_node.parent
+
+    return moves
+
+def export(frontier, time):
+
+    global moves
+
+    moves = backtrace()
+
+    file = open('output.txt', 'w')
+    file.write("Path to goal: " + str(moves))
+    file.write("\nCost to the path: " + str(len(moves)))
+    file.write("\nNumber of visited nodes at the end: " + str(nodes_expanded))
+    file.write("\nRunning time: " + format(time, '.8f'))
+    file.write("\nUsed memory: " + format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1000.0, '.8f'))
+    file.close()
+
+def read(configuration):
+
+    global board_len, board_side
+
+    data = configuration.split(",")
+
+    for element in data:
+        initial_state.append(int(element))
+
+    board_len = len(initial_state)
+
+    board_side = int(board_len ** 0.5)
+
+def main():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('algorithm')
+    parser.add_argument('board')
+    args = parser.parse_args()
+
+    read(args.board)
+
+    function = function_map[args.algorithm]
+
+    start = timeit.default_timer()
+
+    frontier = function(initial_state)
+
+    stop = timeit.default_timer()
+
+    export(frontier, stop-start)
+
+function_map = {
+
+    'ast': ast
+}
+
+if __name__ == '__main__':
+    main()
